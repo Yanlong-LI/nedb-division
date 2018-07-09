c8451d95d12d2156e4daa6699d6b14ee8b9202cd
燕龙 <895185921@qq.com>
2018-07-09 09:11:28 +0800

    初始化项目

---
+++ nedb分离工具/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace nedb分离工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridViewWhere = new System.Windows.Forms.DataGridView();
+            this.filedName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.检查更新ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWhere)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择原始数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 144);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(377, 324);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(511, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "读取数据";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(405, 144);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(371, 324);
+            this.checkedListBox1.TabIndex = 4;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(991, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 38);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "导出数据";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridViewWhere
+            // 
+            this.dataGridViewWhere.AllowUserToAddRows = false;
+            this.dataGridViewWhere.AllowUserToDeleteRows = false;
+            this.dataGridViewWhere.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewWhere.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.filedName,
+            this.Value});
+            this.dataGridViewWhere.Location = new System.Drawing.Point(794, 144);
+            this.dataGridViewWhere.Name = "dataGridViewWhere";
+            this.dataGridViewWhere.RowTemplate.Height = 23;
+            this.dataGridViewWhere.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewWhere.Size = new System.Drawing.Size(297, 324);
+            this.dataGridViewWhere.TabIndex = 6;
+            this.dataGridViewWhere.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewWhere_CellContentClick);
+            // 
+            // filedName
+            // 
+            this.filedName.Frozen = true;
+            this.filedName.HeaderText = "筛选";
+            this.filedName.Name = "filedName";
+            this.filedName.ReadOnly = true;
+            // 
+            // Value
+            // 
+            this.Value.Frozen = true;
+            this.Value.HeaderText = "筛选值";
+            this.Value.Name = "Value";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1103, 25);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem,
+            this.检查更新ToolStripMenuItem});
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.菜单ToolStripMenuItem.Text = "菜单";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // 检查更新ToolStripMenuItem
+            // 
+            this.检查更新ToolStripMenuItem.Name = "检查更新ToolStripMenuItem";
+            this.检查更新ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.检查更新ToolStripMenuItem.Text = "检查更新";
+            this.检查更新ToolStripMenuItem.Click += new System.EventHandler(this.检查更新ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 480);
+            this.Controls.Add(this.dataGridViewWhere);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "nedb分离工具";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWhere)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridViewWhere;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filedName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 检查更新ToolStripMenuItem;
+    }
+}
+
---
+++ nedb分离工具/Form1.cs
@@ -0,0 +1,263 @@
+﻿using nedb;
+using nedb.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace nedb分离工具
+{
+    public partial class Form1 : Form
+    {
+        private List<Dictionary<string, string>> dicc = new List<Dictionary<string, string>>();//<string, Dictionary<string, object>>();
+        public Form1()
+        {
+            InitializeComponent();
+            string exeName = Process.GetCurrentProcess().MainModule.FileName;
+            if(File.Exists(exeName + ".config"))
+            textBox1.Text = Settings.Default.dbPath;
+            else
+            {
+                File.WriteAllText(exeName + ".config", Resources.nedb分离工具_exe);
+                reload();
+            }
+                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件";
+            dialog.Filter = "nedb文件(*.db)|*.db";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+                Settings.Default.dbPath = dialog.FileName;
+                Settings.Default.Save();
+            }
+        }
+        public DataTable StuList()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("序号");
+            dt.Columns.Add("内容");
+            string sFilePath = string.Empty;
+            dicc.Clear();
+            dataGridViewWhere.Rows.Clear();
+            checkedListBox1.Items.Clear();
+
+            ////// 判断选择路径是否存在
+            if (!File.Exists(this.textBox1.Text.ToString()))
+            {
+                MessageBox.Show("选取文件有误，无权限或文件不存在");
+                return dt;
+            }
+
+            try
+            {
+                int i = 0;
+                using (StreamReader sr = new StreamReader(textBox1.Text.ToString()))
+                {
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Trim() == "")
+                        {
+                            continue;
+                        }
+                        //写入数据表
+                        DataRow dr = dt.NewRow();
+                        dr["内容"] = line.ToString();
+                        dr["序号"] = ++i;
+                        dt.Rows.Add(dr);
+
+                        //将json字符串解码为对象
+                        Dictionary<string, object> dic = JsonToDictionary(line);
+                        Dictionary<string, string> dd = new Dictionary<string, string>();
+
+                        foreach(KeyValuePair<string,object> item in dic)
+                        {
+                            dd.Add(item.Key, item.Value.ToString());
+                        }
+
+                        dicc.Add(dd);
+                        foreach(string k in dic.Keys)
+                        {
+                            var type  = dic[k].GetType();
+                            if (!checkedListBox1.Items.Contains(k) && type.Name== "String")
+                            {
+                                checkedListBox1.Items.Add(k);
+                            }
+                        }
+                    }
+                   
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return dt;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = StuList();
+        }
+        /// <summary>
+        /// 将json数据反序列化为Dictionary
+        /// </summary>
+        /// <param name="jsonData">json数据</param>
+        /// <returns></returns>
+        private Dictionary<string, object> JsonToDictionary(string jsonData)
+        {
+            //实例化JavaScriptSerializer类的新实例
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            try
+            {
+                //将指定的 JSON 字符串转换为 Dictionary<string, object> 类型的对象
+                //jss.Deserialize<Dictionary<string, string>>(jsonData);
+                return jss.Deserialize<Dictionary<string, object>>(jsonData);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string name = this.textBox1.Text;  //文件名
+            string path = string.Empty;  //文件路径
+            SaveFileDialog save = new SaveFileDialog();
+            save.Title = "请选择保存路径";
+            save.Filter = "nedb文件(*.db)|*.db";
+
+            if (save.ShowDialog() == DialogResult.OK)
+                path = save.FileName;
+            else
+                return;
+
+            
+
+
+            using (System.IO.FileStream file = new System.IO.FileStream(path, System.IO.FileMode.Create, System.IO.FileAccess.Write))
+            {
+                using (System.IO.TextWriter text = new System.IO.StreamWriter(file, System.Text.Encoding.Default))
+                {
+                    int i = 0;
+                    foreach (Dictionary<string,string> item in dicc)
+                    {
+                        Boolean write = true;
+                        foreach (DataGridViewRow row in dataGridViewWhere.Rows)
+                        {
+                            var lamada = GetWherePredicate(row);
+                            var dc = item.Where(lamada.Compile()).FirstOrDefault();
+                            if (dc.Key == null)
+                            {
+                                write = false;
+                                break;
+                            }
+                            
+                        }
+                        if(write)
+                        text.WriteLine(dataGridView1.Rows[i].Cells["内容"].Value);
+                        i++;
+                    }
+
+                }
+            }
+        }
+        Expression<Func<KeyValuePair<string,string>, bool>> GetWherePredicate(DataGridViewRow row)
+        {
+            var p = Expression.Parameter(typeof(KeyValuePair<string, string>), "p");
+            var expr1 = Expression.Equal(Expression.Constant(1), Expression.Constant(1));
+            //foreach(DataGridViewRow row in dataGridViewWhere.Rows) { 
+                try
+                {
+                    Dictionary<string, string> tt = new Dictionary<string, string>();
+                    tt.Add(row.Cells[0].Value.ToString(), row.Cells[1].Value.ToString());
+                    var expr2 = Expression.Equal(Expression.Property(p, "Key"), Expression.Constant(tt.First().Key));
+                    var expr3 = Expression.Equal(Expression.Property(p, "Value"), Expression.Constant(tt.First().Value));
+
+                    expr3 = Expression.And(expr2, expr3);
+                    expr1 = Expression.And(expr1, expr3);
+                }
+                catch (Exception e) {
+                    MessageBox.Show("你选择的筛选条件："+row.Cells[0].Value.ToString()+"，必须填写筛选值");
+                }
+            //}
+            var expr4 = Expression.Lambda<Func<KeyValuePair<string, string>, bool>>(expr1, p);
+            return expr4;
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            string fieldName = checkedListBox1.Items[e.Index].ToString();
+
+            if (e.NewValue== System.Windows.Forms.CheckState.Checked) { 
+            
+            dataGridViewWhere.Rows.Add(fieldName,"");
+            }
+            else
+            {
+               foreach(DataGridViewRow row in dataGridViewWhere.Rows)
+                {
+                    
+                    if ((string)row.Cells[0].Value == fieldName)
+                    {
+                        dataGridViewWhere.Rows.Remove(row);
+                    }
+                }
+            }
+        }
+
+        private void dataGridViewWhere_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            this.dataGridViewWhere.CurrentCell = this.dataGridViewWhere.Rows[e.RowIndex].Cells[1];//获取当前单元格
+            this.dataGridViewWhere.BeginEdit(true);//将单元格设为编辑状态
+        }
+        private about about;
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (about == null || about.IsDisposed == true)
+                about = new about();
+            //this.Hide();//隐藏现在这个窗口
+            about.Show();//新窗口显现
+            about.Activate();
+        }
+
+        private void 检查更新ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你当前使用为离线版本，无法进行自升级。");
+        }
+        private void reload()
+        {
+            Application.ExitThread();
+            Thread thtmp = new Thread(new ParameterizedThreadStart(run));
+            object appName = Application.ExecutablePath;
+            Thread.Sleep(1);
+            thtmp.Start(appName);
+        }
+        private void run(Object obj)
+        {
+            Process ps = new Process();
+            ps.StartInfo.FileName = obj.ToString();
+            ps.Start();
+            Application.Exit();
+        }
+    }
+  
+}
